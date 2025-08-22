499338ad9a867c6561fe19f4b818b522d0f93005
kmykprn <kmykprn@gmail.com>
2025-08-22 15:49:42 +0900

    /runpod-volume/models/に重みをダウンロードするように修正

--- models/download_hunyuan3d_weights.sh
+++ models/download_hunyuan3d_weights.sh
@@ -1,7 +1,7 @@
 python3 -c "
 from huggingface_hub import snapshot_download
-path1 = snapshot_download('tencent/Hunyuan3D-2mini', allow_patterns=['hunyuan3d-dit-v2-mini-fast/*'], cache_dir='.')
-path2 = snapshot_download('tencent/Hunyuan3D-2', allow_patterns=['hunyuan3d-paint-v2-0-turbo/*'], cache_dir='.')
-path3 = snapshot_download('tencent/Hunyuan3D-2', allow_patterns=['hunyuan3d-delight-v2-0/*'], cache_dir='.')
+path1 = snapshot_download('tencent/Hunyuan3D-2mini', allow_patterns=['hunyuan3d-dit-v2-mini-fast/*'], cache_dir='/runpod-volume/models/')
+path2 = snapshot_download('tencent/Hunyuan3D-2', allow_patterns=['hunyuan3d-paint-v2-0-turbo/*'], cache_dir='/runpod-volume/models/')
+path3 = snapshot_download('tencent/Hunyuan3D-2', allow_patterns=['hunyuan3d-delight-v2-0/*'], cache_dir='/runpod-volume/models/')
 print('Downloaded to:', path1, path2, path3)
 "
